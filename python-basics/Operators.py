3f7727082494d09ee9a6b7bef95b46f37fac8af9
anushka23Kalal <anushkasampathkalal@gmail.com>
2026-04-10 15:21:06 +0530

    Added operators.py file

---
+++ python-basics/Operators.py
@@ -0,0 +1,39 @@
+
+# -------- 1. ARITHMETIC OPERATORS --------
+a = 10
+b = 3
+
+print("Arithmetic Operators:")
+print("Addition:", a + b)        # 10 + 3 = 13
+print("Subtraction:", a - b)     # 10 - 3 = 7
+print("Multiplication:", a * b)  # 10 * 3 = 30
+print("Division:", a / b)        # 10 / 3 = 3.33
+print("Modulus:", a % b)         # remainder = 1
+print("Power:", a ** b)          # 10^3 = 1000
+print("Floor Division:", a // b) # 10 // 3 = 3
+
+print("\n---------------------\n")
+
+# -------- 2. COMPARISON OPERATORS --------
+print("Comparison Operators:")
+print("a == b:", a == b)   # False
+print("a != b:", a != b)   # True
+print("a > b:", a > b)     # True
+print("a < b:", a < b)     # False
+print("a >= b:", a >= b)   # True
+print("a <= b:", a <= b)   # False
+
+print("\n---------------------\n")
+
+# -------- 3. LOGICAL OPERATORS --------
+x = True
+y = False
+
+print("Logical Operators:")
+print("x and y:", x and y) # False
+print("x or y:", x or y)   # True
+print("not x:", not x)     # False
+
+print("\n---------------------\n")
+
+
