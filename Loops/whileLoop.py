9c1caf37c42de864a99571a9a54fdcb7f3c5df81
anushka23Kalal <anushkasampathkalal@gmail.com>
2026-04-21 15:01:40 +0530

    while loop example

---
+++ Loops/whileLoop.py
@@ -0,0 +1,30 @@
+#sum of first 100 natural numbers
+sum=0
+i=1
+while i<=100:
+    sum=sum+i
+    i=i+1
+print(sum)
+
+#print odd numbers from 1 to 20
+i=1
+while i<=20:
+    if i%2!=0:
+        print(i)
+    i=i+1
+
+#print each character of a string in a new line
+name=input("Enter student name: ")
+i=0
+while i<len(name):
+    print(name[i])
+    i=i+1
+
+#print numbers from 1 to 29
+i=1
+while i<30:
+    print(i)
+    i=i+1
+
+
+
